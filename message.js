51c61ca28e6458f53a949a49ec38310a4a2cdf51
shinshilkina <anastasiashilina98@gmail.com>
2020-08-20 23:04:53 +0700

    добавила классы в message.js

--- message.js
+++ message.js
@@ -1,41 +1,55 @@
-function changeMessageText(message) {
-    const messageTag = document.querySelector('.modal-body-message');
-    messageTag.textContent = message;
-    return true;
-}
+class Message {
 
-function generatePopUp(message) {
-    let xmlHttp = null;
-    xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", chrome.extension.getURL ("message.html"), false );
-    xmlHttp.send( null );
-    let inject = document.createElement("div");
-    inject.innerHTML = xmlHttp.responseText
-    document.body.insertBefore (inject, document.body.firstChild);
+    generatePopUp() {
+        let xmlHttp = null;
+        xmlHttp = new XMLHttpRequest();
+        xmlHttp.open( "GET", chrome.extension.getURL ("message.html"), false );
+        xmlHttp.send( null );
+        let inject = document.createElement("div");
+        inject.innerHTML = xmlHttp.responseText
+        document.body.insertBefore (inject, document.body.firstChild);
+        return true;
+    }
 
-    changeMessageText(message);
-
-    return true;
-}
-
-function closeButtonAction() {
-    return document.querySelector('.btn-secondary').addEventListener('click', () => {
-        document.querySelector('.modal').remove();
-        chrome.runtime.sendMessage({cmd: 'buttonClicked', domain: location.hostname},
-            (message) => {
-            console.log(message);
+    closeButtonAction() {
+        return document.querySelector('.btn-secondary').addEventListener('click', () => {
+            document.querySelector('.modal').remove();
+            chrome.runtime.sendMessage({cmd: 'buttonClicked', domain: location.hostname},
+                (message) => {
+                    console.log(message);
+                });
         });
-    });
+    }
+
+    addStyle() {
+        return document.head.insertAdjacentHTML('beforeend',
+            '<link rel="stylesheet" type="text/css" href="' +
+            chrome.runtime.getURL("message.css") + '">');
+    }
 }
+
+class ChangeMessage {
+    constructor(message) {
+        this.message = message;
+    }
+
+    changeText(message) {
+        const messageTag = document.querySelector('.modal-body-message');
+        messageTag.textContent = message;
+        return true;
+    }
+}
+
 
 chrome.runtime.sendMessage({cmd: 'getMessage', domain: location.hostname},
     (message) => {
         if (message) {
-            generatePopUp(message);
-            document.head.insertAdjacentHTML('beforeend',
-                '<link rel="stylesheet" type="text/css" href="' +
-                chrome.runtime.getURL("message.css") + '">');
+            let currentMessage = new Message();
+            currentMessage.generatePopUp();
+            currentMessage.addStyle();
+            currentMessage.closeButtonAction();
 
-            closeButtonAction();
+            let changeMessage = new ChangeMessage(message);
+            changeMessage.changeText(message);
         }
     });
