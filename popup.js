32f46c0e9b9de24a03d3652c94b8054820d40888
shinshilkina <anastasiashilina98@gmail.com>
2020-08-21 00:07:59 +0700

    add class to popup.js structure

--- popup.js
+++ popup.js
@@ -1,34 +1,49 @@
-const body = document.querySelector('body');
+class List {
+    constructor(listElements) {
+        this.listElements = listElements;
+    }
 
-function createList(elem) {
-    const container = document.createElement('ul');
-    container.className = 'list-group';
+    createList(listElements) {
+        const container = document.createElement('ul');
+        container.className = 'list-group';
 
-    const node = document.createElement('li');
-    node.className = 'list-group-item';
-    const textNode = document.createTextNode(elem.name);
-    node.href = 'https://' + elem.domain;
-    node.appendChild(textNode);
-    node.addEventListener('click', () => clickUrl(node.href));
-    container.append(node);
+        const node = document.createElement('li');
+        node.className = 'list-group-item';
+        const textNode = document.createTextNode(listElements.name);
+        node.href = 'https://' + listElements.domain;
+        node.appendChild(textNode);
+        node.addEventListener('click', () => {
+            const listActions = new ListActions(node.href);
+            listActions.clickUrl(listActions.newTabUrl);
+        });
+        container.append(node);
 
-    body.appendChild(container);
+        document.body.appendChild(container);
+    }
 }
+
+class ListActions {
+    constructor(newTabUrl) {
+        this.newTabUrl = newTabUrl;
+    }
+
+    clickUrl(newTabUrl) {
+        chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
+            chrome.tabs.update({
+                url: newTabUrl
+            });
+        });
+        return true;
+    }
+}
+
 
 document.addEventListener('DOMContentLoaded', function () {
     chrome.runtime.sendMessage({cmd: "list"}, (data) => {
         const keys = Object.keys(data);
         for (let key of keys) {
-            createList(data[key]);
+            const newList = new List(data[key]);
+            newList.createList(newList.listElements);
         }
     });
 });
-
-function clickUrl(new_url) {
-    chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
-        chrome.tabs.update({
-            url: new_url
-        });
-    });
-    return false;
-}
